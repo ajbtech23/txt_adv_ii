2fdff287e7a5ad82138850ed6d7137baac122063
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-05 20:38:52 +0100

    Exploring variations of for loops

--- mop_the_floors.py
+++ mop_the_floors.py
@@ -1,5 +1,23 @@
+def pretty_print(arr):
+    for item in arr:
+        print(f"*. {item}")
+    print("\nThank you for shopping with us :D!")
+
 favorites = []
 more_items = True
+
+def prettier_print(arr):
+    i = 0
+    for item in arr:
+        print(f"{i + 1}. {item}")
+        i += 1
+    print("\nYou look oh so fly XD")
+
+def prettier_still(arr):
+    for i in range(len(arr)):
+        print(f"{i + 1}. ==> {arr[i]}")
+    print("\nThey almost had me but I hustled my way through...\n")
+
 
 while more_items:
     user_input = input("Add something to your shopping basket:\n")
@@ -15,3 +33,10 @@ while more_items:
 print("Here are all of the lovely things you have bought today:\n")
 for index, fave in enumerate(favorites, 1):
     print(f"{index}. ==> {fave}")
+
+print("")
+pretty_print(favorites)
+print("")
+prettier_print(favorites)
+print("")
+prettier_still(favorites)
