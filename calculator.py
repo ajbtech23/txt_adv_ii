83127fd980b7777b010abed6db3aaa7540edd694
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-06 14:40:07 +0100

    Built a decent calculator program in python

--- calculator.py
+++ calculator.py
@@ -22,8 +22,11 @@ def get_user_int_values():
         i += 1
     return arr_user_int_values
 
+def get_user_calc_choice():
+    return input("Would you like to add, subtract, multiply or divide? You can also use this as a chance to close the app...\n")
+
 def get_user_cmd():
-    return input("Press 'Q' to quit the calculator otherwise lets have some fun with math XD")
+    return input("Press 'Q' to quit the calculator or press 'GO' and lets 'GO' have some fun with math XD\n")
 
 def machine_calculator():
 
@@ -37,3 +40,19 @@ def machine_calculator():
             user_is_active = False
         else:
             arr_user_int_values = get_user_int_values()
+            str_user_calc_choice = get_user_calc_choice()
+
+            if str_user_calc_choice == 'add':
+                print(f"The sum of {arr_user_int_values[0]} and {arr_user_int_values[1]} is {calc_sum(arr_user_int_values[0], arr_user_int_values[1])}")
+            elif str_user_calc_choice == 'subtract':
+                print(f"The difference between {arr_user_int_values[0]} and {arr_user_int_values[1]} is {calc_difference(arr_user_int_values[0], arr_user_int_values[1])}")
+            elif str_user_calc_choice == 'multiply':
+                print(f"The product of {arr_user_int_values[0]} and {arr_user_int_values[1]} is {calc_product(arr_user_int_values[0], arr_user_int_values[1])}")
+            elif str_user_calc_choice == 'divide':
+                print(f"The output from the division of {arr_user_int_values[0]} by {arr_user_int_values[1]} is {calc_div_output(arr_user_int_values[0], arr_user_int_values[1])}")
+            elif str_user_calc_choice == 'exponents':
+                print(f"The output of applying {arr_user_int_values[1]} is an exponent onto the base {arr_user_int_values[0]} is {calc_exponents_output(arr_user_int_values[0], arr_user_int_values[1])}")
+            else:
+                print("Try again...")
+
+machine_calculator()
