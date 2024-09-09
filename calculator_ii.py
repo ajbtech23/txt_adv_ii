cf23fd3cdf75f90bb61ee64516e8f8d874acf180
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-09 21:36:47 +0100

    PND 4

--- calculator_ii.py
+++ calculator_ii.py
@@ -14,36 +14,46 @@ def calc_powr(arr_values):
     return (arr_values[0] ** arr_values[1])
 
 def play():
-    arr_values = []
 
-    while len(arr_values) < 2:
-        try:
-            user_input_value = int(input("Give us a number:\n> "))
-            arr_values.append(user_input_value)
-        except ValueError:
-            print(f"Your last input will not work in our calculator... try again!")
+    user_calc_active = True
 
-    print("You have given us the following values to work with:\n")
-    for val in arr_values:
-        print(f"We will be working with: {val}")
+    while user_calc_active:
+        str_user_calc_status = input("\nWelcome to CalcuME; would you like to use the calculator? Enter 'YES' to proceed and anything else to exit:\n> ")
+        if str_user_calc_status.lower() == 'yes':
+            arr_values = []
 
-    print("What calculation would you like to perform? You can:\n Add; Subtract; Multiply; Divide; Calculate the results from an 'Exponent' application...")
-    user_calc_choice = input("Your choice is:\n> ")
+            while len(arr_values) < 2:
+                try:
+                    user_input_value = int(input("Give us a number:\n> "))
+                    arr_values.append(user_input_value)
+                except ValueError:
+                    print(f"Your last input will not work in our calculator... try again!")
 
-    if user_calc_choice in ['q', 'Q', 'quit', 'Quit']:
-        print("Thanks for playing :D ta ta!")
-    elif user_calc_choice == 'Add':
-        print(f"The sum of {arr_values[0]} and {arr_values[1]} is {calc_total(arr_values)}")
-    elif user_calc_choice == 'Subtract':
-        print(f"The difference between {arr_values[0]} and {arr_values[1]} is {calc_diff(arr_values)}")
-    elif user_calc_choice == 'Multiply':
-        print(f"The product of {arr_values[0]} and {arr_values[1]} is {calc_prod(arr_values)}")
-    elif user_calc_choice == 'Divide':
-        print(f"The resulting output from the division of {arr_values[0]} and {arr_values[1]} is {calc_divd(arr_values)}")
-    elif user_calc_choice == 'Exponents':
-        print(f"The resulting output from applying {arr_values[1]} as an exponent onto {arr_values[0]} is {calc_powr(arr_values)}")
-    else:
-        print("We couldn't make sense of your chosen calculation I'm afraid...")
+            print("You have given us the following values to work with:\n")
+            for val in arr_values:
+                print(f"We will be working with: {val}")
+
+            print("What calculation would you like to perform? You can:\n Add; Subtract; Multiply; Divide; Calculate the results from an 'Exponent' application... Enter 'Q' to quit...")
+            user_calc_choice = input("Your choice is:\n> ")
+
+            if user_calc_choice in ['q', 'Q', 'quit', 'Quit']:
+                print("Thanks for playing :D ta ta!")
+                user_calc_active = False
+            elif user_calc_choice == 'Add':
+                print(f"The sum of {arr_values[0]} and {arr_values[1]} is {calc_total(arr_values)}")
+            elif user_calc_choice == 'Subtract':
+                print(f"The difference between {arr_values[0]} and {arr_values[1]} is {calc_diff(arr_values)}")
+            elif user_calc_choice == 'Multiply':
+                print(f"The product of {arr_values[0]} and {arr_values[1]} is {calc_prod(arr_values)}")
+            elif user_calc_choice == 'Divide':
+                print(f"The resulting output from the division of {arr_values[0]} and {arr_values[1]} is {calc_divd(arr_values)}")
+            elif user_calc_choice == 'Exponents':
+                print(f"The resulting output from applying {arr_values[1]} as an exponent onto {arr_values[0]} is {calc_powr(arr_values)}")
+            else:
+                print("We couldn't make sense of your chosen calculation I'm afraid...")
+        else:
+            print("It's been a pleasure; goodbye :D")
+            user_calc_active = False
 
 
 play()
