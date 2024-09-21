66377702ce8929680fa27071cd785c5dcfe212e7
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-22 00:07:53 +0100

    Still working

---
+++ elmagico.py
@@ -0,0 +1,2 @@
+print("Unironically had one of my best days completed all of the Foundation Material for Northern Coders")
+print("As well as activating my account for the Google Cloud Data Engineering Programme by completing Labs")
